b71aec5f11e66877459a5ebf865565e2a397b887
cdt <cdt.eth@gmail.com>
2025-01-20 20:28:55 -0500

    delete dir

---
+++ internal/templates/auth.go
@@ -0,0 +1,15 @@
+package templates
+
+const (
+    SignupFormHTML = `
+    <div>
+        <form id="signup-form"...>
+        </form>
+    </div>`
+
+    LoginFormHTML = `
+    <div>
+        <form id="login-form"...>
+        </form>
+    </div>`
+) 
